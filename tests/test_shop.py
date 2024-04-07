15224cd610e08539db39c4db25448d6890d6603b
Elizaveta Kazova <e.a.kazova@gmail.com>
2024-04-07 14:40:54 +0500

    Изменены названия переменных для читаемости

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -44,6 +44,7 @@ class TestCart:
 
     def test_cart_add_product(self, milk, cookie):
         cart = Cart()
+
         cart.add_product(milk)
         cart.add_product(cookie, 2)
         cart.add_product(cookie, 1)
@@ -51,7 +52,7 @@ class TestCart:
         assert cart.products == {milk: 1, cookie: 3}
 
     def test_cart_remove_product(self, filled_cart, milk, cookie, cat_food):
-        cat_food_start_quantity = filled_cart.products[cat_food]
+        cat_food_cart_quantity = filled_cart.products[cat_food]
 
         filled_cart.remove_product(milk)
         filled_cart.remove_product(cookie, filled_cart.products[cookie] + 1)
@@ -59,7 +60,7 @@ class TestCart:
 
         assert milk not in filled_cart.products
         assert cookie not in filled_cart.products
-        assert filled_cart.products[cat_food] == cat_food_start_quantity - 1
+        assert filled_cart.products[cat_food] == cat_food_cart_quantity - 1
 
     def test_cart_clear(self, filled_cart):
         filled_cart.clear()
@@ -67,35 +68,35 @@ class TestCart:
         assert not filled_cart.products
 
     def test_cart_get_total_price(self, filled_cart, milk, cookie, cat_food):
-        milk_total_price = milk.price * filled_cart.products[milk]
-        cookie_total_price = cookie.price * filled_cart.products[cookie]
-        cat_food_total_price = cat_food.price * filled_cart.products[cat_food]
+        milk_cart_price = milk.price * filled_cart.products[milk]
+        cookie_cart_price = cookie.price * filled_cart.products[cookie]
+        cat_food_cart_price = cat_food.price * filled_cart.products[cat_food]
 
-        assert filled_cart.get_total_price() == milk_total_price + cookie_total_price + cat_food_total_price
+        assert filled_cart.get_total_price() == milk_cart_price + cookie_cart_price + cat_food_cart_price
 
-    def test_cart_get_total_price_without_products(self, filled_cart, milk, cookie, cat_food):
+    def test_cart_get_total_price_without_products(self):
         cart = Cart()
 
         assert cart.get_total_price() == 0
 
     def test_cart_buy(self, filled_cart, milk, cookie, cat_food):
-        milk_start_total_quantity = milk.quantity
-        cookie_start_total_quantity = cookie.quantity
-        cat_food_start_total_quantity = cat_food.quantity
+        milk_stock_quantity = milk.quantity
+        cookie_stock_quantity = cookie.quantity
+        cat_food_stock_quantity = cat_food.quantity
 
-        milk_start_cart_quantity = filled_cart.products[milk]
-        cookie_start_cart_quantity = filled_cart.products[cookie]
-        cat_food_start_cart_quantity = filled_cart.products[cat_food]
+        milk_cart_quantity = filled_cart.products[milk]
+        cookie_cart_quantity = filled_cart.products[cookie]
+        cat_food_cart_quantity = filled_cart.products[cat_food]
 
         filled_cart.buy()
 
-        assert milk.quantity == milk_start_total_quantity - milk_start_cart_quantity
-        assert cookie.quantity == cookie_start_total_quantity - cookie_start_cart_quantity
-        assert cat_food.quantity == cat_food_start_total_quantity - cat_food_start_cart_quantity
+        assert milk.quantity == milk_stock_quantity - milk_cart_quantity
+        assert cookie.quantity == cookie_stock_quantity - cookie_cart_quantity
+        assert cat_food.quantity == cat_food_stock_quantity - cat_food_cart_quantity
 
-    def test_cart_buy_not_enough_products(self, milk):
+    def test_cart_buy_not_enough_products(self, product):
         cart = Cart()
-        cart.add_product(milk, milk.quantity + 1)
+        cart.add_product(product, product.quantity + 1)
 
         with pytest.raises(ValueError):
             cart.buy()
